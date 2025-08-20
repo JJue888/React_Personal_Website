2835e2e98dc3a950fcdddf3dd56857f8c11c6960
JJue888 <joshuajue888@gmail.com>
2025-08-20 01:33:06 -0700

    Added experience cards, working on details page

--- app/routes/experiences.tsx
+++ app/routes/experiences.tsx
@@ -1,5 +1,30 @@
+import {Link} from "react-router";
+import WorkDisplay from "~/components/WorkDisplay";
+// @ts-ignore
+import { EXPERIENCE_DATA } from "~/data/experience.js";
+
 function Experiences() {
-    return <h1>Experience</h1>
+
+    return (
+        <>
+            <div>
+                <header className="w-full py-6 text-center  dark:bg-gray-950">
+                    <h1 className="text-3xl font-bold text-black dark:text-white">Projects</h1>
+                </header>
+                <div>
+                    <ul className="flex flex-wrap gap-4 justify-evenly pb-2">
+                        {EXPERIENCE_DATA.map((project: { title: string; image: string[]; }) => (
+                            <li key={project.title} className="w-[40vw] bg-gray-950 p-4 rounded-lg shadow-md">
+                                <Link to={project.title}>
+                                    <WorkDisplay {...project} />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Experiences;
