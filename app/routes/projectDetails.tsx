4554fc41c7a0583f8698a01370c44fbca35d9c9b
JJue888 <joshuajue888@gmail.com>
2025-08-20 01:32:58 -0700

    Added project cards, working on details page

--- app/routes/projectDetails.tsx
+++ app/routes/projectDetails.tsx
@@ -1,5 +1,21 @@
+import {useParams} from "react-router";
+// @ts-ignore
+import { PROJECT_DATA } from "~/data/projects";
+
 function ProjectDetails() {
-    return <h1>Projects</h1>
+    const params = useParams();
+    const foundProject : { title: string; description: string[], image: string[]; } = PROJECT_DATA.find((project: { title: string | undefined; })   => project.title === params.projectTitle);
+    return (
+        <>
+            <h1>Projects</h1>
+            <p>{params.projectTitle}</p>
+            <img src={foundProject.image[0]} alt={params.projectTitle} />
+            <ul>
+                {foundProject.description.map((description: string, index: number) => (
+                    <p key={index}>{description}</p>
+                ))}
+            </ul>
+        </>)
 }
 
 export default ProjectDetails;
--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,5 +1,29 @@
+// @ts-ignore
+import { PROJECT_DATA } from "~/data/projects";
+import WorkDisplay from "~/components/WorkDisplay";
+import {Link} from "react-router";
+
 function Projects() {
-    return (<h1>Test</h1>)
+    return (
+        <>
+            <div>
+                <header className="w-full py-6 text-center  dark:bg-gray-950">
+                    <h1 className="text-3xl font-bold text-black dark:text-white">Projects</h1>
+                </header>
+                <div>
+                    <ul className="flex flex-wrap gap-4 justify-evenly pb-2">
+                        {PROJECT_DATA.map((project: { title: string; image: string[]; }) => (
+                            <li key={project.title} className="w-[40vw] bg-gray-950 p-4 rounded-lg shadow-md">
+                                <Link to={project.title}>
+                                    <WorkDisplay {...project} />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Projects;
